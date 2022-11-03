cd74e80f5ecf2efac86896e3e50d58cc3db15ce3
LifesMystery <arjunvsudn@gmail.com>
2022-11-02 20:41:43 -0500

    hi

--- Casino/Hand.java
+++ Casino/Hand.java
@@ -42,7 +42,7 @@ public class Hand{
         return cardOut;
     }
     
-    /*public Card removeCard(Card cardOut){
+   /*  public Card removeCard(Card cardOut){
         for(int i = 0; i < cards.length; i++){
             if(cards[i].getCard() == cardOut.getCard() && cards[i].getSuit() == cardOut.getSuit())
                 
