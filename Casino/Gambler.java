94d4b02c0f176c86b2a2e8fe43171e09a0153a46
Coby5735 <codyplaysvr@gmail.com>
2022-11-03 11:30:05 -0500

    Gamblerv2

---
+++ Casino/Gambler.java
@@ -0,0 +1,46 @@
+public class Gambler{
+    double totalMoney;
+    int wins;
+    int losses;
+    DynamicHand hand;
+
+    public Gambler(DynamicHand _hand, double startingMoney){
+        hand = _hand;
+        totalMoney = startingMoney;
+        wins = 0;
+        losses = 0;
+    }
+
+    //Getters
+    public DynamicHand getHand(){
+        return hand;
+    }
+    public int getWins(){
+        return wins;
+    }
+    public int getLosses(){
+        return losses;
+    }
+    public double getMoney(){
+        return totalMoney;
+    }
+    
+    //Setters
+    public void win(int moneyWon){
+        wins++;
+        totalMoney += moneyWon;
+    }
+    public void win(int moneyWon, int numberOfWins){
+        wins += numberOfWins;
+        totalMoney += moneyWon;
+    }
+    public void lose(int moneyLost){
+        losses ++;
+        totalMoney -= moneyLost;
+    }
+    public void lose(int moneyLost, int numberOfLosses){
+        losses += numberOfLosses;
+        totalMoney -= moneyLost;
+    }
+
+}
