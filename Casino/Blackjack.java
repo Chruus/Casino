eb0df0fb9d21216f783ff42adda2fa87490aa041
LifesMystery <arjunvsudn@gmail.com>
2022-11-03 11:31:34 -0500

    netcode wip

--- Casino/Blackjack.java
+++ Casino/Blackjack.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-import java.security.cert.TrustAnchor;
+
 
 
 public class Blackjack {
--- Casino/Casino.java
+++ Casino/Casino.java
@@ -3,6 +3,7 @@
 import static java.lang.System.out;
 import java.io.*;
 import java.util.*;
+import java.net.*;
 
 public class Casino{
     public static void main(String[] args) throws IOException{
@@ -43,7 +44,92 @@ public class Casino{
         }
         
         out.println("Thanks for playing at the casino!");
-    }
+
+
+
+
+
+        
+        
+        /* 
+		String sentence = "";
+		String IP;
+		int portNum = 7777;
+		String answer;
+		Scanner scan = new Scanner(System.in);
+		Socket clientSocket;
+		
+		Socket connectionSocket;
+
+		System.out.println("Welcome to the Casino!  You are at Casino #" + portNum);
+		System.out.println("Are you a player? (Y or N)");
+		//System.out.println("Do you want to send a message right now? (Y or N)?");
+
+		answer = scan.nextLine();
+
+		if(answer.equalsIgnoreCase("y"))
+		{
+			System.out.println("What is the IP address of Casino you want to connect to? (ask your dealer for help if needed)");
+			IP = scan.nextLine();
+
+			while(true)
+            {
+
+                clientSocket = new Socket(IP, portNum);
+                DataOutputStream outToServer =
+                                    new DataOutputStream(clientSocket.getOutputStream());
+                Scanner inFromServer =
+                                    new Scanner(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("Enter Your Message: ");
+                sentence = scan.nextLine();
+
+                System.out.println("Waiting for reply...");
+
+                outToServer.writeBytes(sentence + '\n');
+
+                sentence = inFromServer.readLine();
+
+                System.out.println("Message from " + clientSocket.getInetAddress() +
+                       ": " + sentence);
+            }
+		}
+
+		else
+		{
+			String clientSentence;
+			String reply;
+
+			ServerSocket welcomeSocket = new ServerSocket(portNum);
+			System.out.println("Ok, you are online and waiting...");
+
+			while(true)
+			{
+				connectionSocket = welcomeSocket.accept();
+
+				BufferedReader inFromClient =
+					new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+				DataOutputStream outToClient =
+					new DataOutputStream(connectionSocket.getOutputStream());
+
+				clientSentence = inFromClient.readLine();
+
+				System.out.println("Message from " + connectionSocket.getInetAddress() +
+									": " + clientSentence);
+				reply = maggie.getResponse(clientSentence);
+			    System.out.println("Your Reply: " + reply);
+			    
+
+			    outToClient.writeBytes(reply + '\n');
+			}
+
+
+		}
+
+
+
+	
+    }*/
 }
 
 
